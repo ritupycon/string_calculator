04f2eec3c52a8a55f658570214a4d223c376ce0d
Ritu <ritu31195@gmail.com>
2025-02-10 04:08:11 +0530

    chore: Add a testcase - empty string should return 0

---
+++ tests/test_string_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.string_calculator import StringCalculator
+
+class TestStringCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.calc = StringCalculator()
+    
+    def test_empty_string(self):
+        self.assertEqual(self.calc.add(""), 0)
+
+if __name__ == '__main__':
+    unittest.main()
